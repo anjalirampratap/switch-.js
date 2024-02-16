3eb76a805cbaa015ea4fcc427d9ad3248416c5bb
anjalirampratap <151140289+anjalirampratap@users.noreply.github.com>
2024-02-16 15:00:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+age= prompt("enter your name")
+if(age=18){
+    
+    document.write("eliible for vote","<br>")
+}
+else{
+    document.write("not eligible for vote","<br>")
+}
+number=prompt("enter your number")
+if(number%2==0){
+document.write("even number","<br>")
+}
+else{
+    document.write("odd number","<br>")
+}    
+question;3
+
+value=prompt("enter your value")
+if(valve>0){
+document.write("postive valve")
+
+}
+else{
+    document.write("negetive valve")
+}
+question;4
+
+marksl=prompt("enter your marksl")
+marks2=prompt("enter your marks2")
+if(marksl+marks2>=100){
+    document.write("pass")
+}
+else{
+    document.write("fail")
+}
---
+++ switch.js
@@ -0,0 +1,86 @@
+// day=parseInt(prompt("enter your case"))
+// document.write(day)
+// switch(day){
+// case 1:
+//     document.write(monday)
+//     break;
+// case 2:
+//      document.write(Tuesday)
+//      break;
+// case 3:
+//     document.write(wednesday)
+//     break;
+// case 4:
+//     document.write(Thursday)
+//     break;
+// case 5:
+//     document.write(Friday)
+//     break;
+// case 6:
+//     document.write(saturday)
+//     break;
+// case 7:
+//     document.write(sunday)    
+// default:
+//     document.write("enter invalid no.")
+// }
+// a=porseint(prompt("enter your fruit name"))
+// switch(b){
+// case "mango":
+//     document.write("green")
+//     break;
+// case "apple":
+//     document.write("red")
+//     break;    
+// case "orenge":
+//     document.write("oremge")
+//     break;
+// case "litch":
+//     document.write("pink")
+//     break;
+// case "banana":
+//     document.write("yellow")
+//     default:
+//     document.write("vegetable name")
+// }
+month=parseInt(prompt("enter your number"))
+switch(month){
+case 1:
+    document.write("january")
+    break;
+case 2:
+    document.write("february")
+    break;
+case 3:
+    document.write("march")
+    break;
+case 4:
+    document.write("april")
+    break;
+case 5:
+    document.write("may")
+    break;
+case 6:
+    document.write("june")
+    break;
+case 7:
+    document.write("july")
+    break;
+case 8:
+    document.write("august")
+    break;
+case 9:
+    document.write("september")
+    break;
+case 10:
+    document.write("October")
+    break;
+case 11:
+    document.write("november")
+    break;
+case 12:
+    document.write("december")
+    break;
+    default:
+        document.writr("Enter corret month name")
+}
